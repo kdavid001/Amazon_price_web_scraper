3425fc373f9eb54de8fadeef0034a21d688d8e24
David Ogunmola <127635293+kdavid001@users.noreply.github.com>
2024-09-17 21:51:48 +0100

    Update live_amazon_web_scraping.py

--- live_amazon_web_scraping.py
+++ live_amazon_web_scraping.py
@@ -16,15 +16,7 @@ site_url = ("https://www.amazon.com/Sony-WH-1000XM5-Canceling-Headphones-Hands-F
 
 
 headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-GB,en;q=0.9",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15"
-                      " (KHTML, like Gecko) "
-                      "Version/17.5 Safari/605.1.15",
+        #get your header by going to thid link: http://myhttpheader.com/
 }
 soup = BeautifulSoup(requests.get(site_url).text, "html.parser")
 name = soup.find(id="productTitle").text
